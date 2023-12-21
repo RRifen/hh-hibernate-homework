f3cac5d33618aa951713385a2ce200f95f998251
Arseny Bazhenov <gorbaty.kit@gmail.com>
2023-12-21 21:30:54 +0300

    solve tasks

--- src/main/java/ru/hh/school/entity/Area.java
+++ src/main/java/ru/hh/school/entity/Area.java
@@ -1,7 +1,13 @@
 package ru.hh.school.entity;
 
+import javax.persistence.*;
+
 //TODO: оформите entity
+@Entity
 public class Area {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "area_id")
   private Integer id;
 
   private String name;
