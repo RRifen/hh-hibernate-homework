ecd08dee0061d5ab3ec82a870525935d952daba6
Arseny Bazhenov <arsn.bazhenov@gmail.com>
2023-12-22 14:30:10 +0300

    fix SequenceGenerator in Resume entity

--- src/main/java/ru/hh/school/entity/Resume.java
+++ src/main/java/ru/hh/school/entity/Resume.java
@@ -16,7 +16,7 @@ public class Resume {
 
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "resume_generator")
-  @SequenceGenerator(name = "resume_generator", sequenceName = "resume_id_seq")
+  @SequenceGenerator(name = "resume_generator", sequenceName = "resume_id_seq", allocationSize = 10)
   private Integer id;
 
   private String description;
